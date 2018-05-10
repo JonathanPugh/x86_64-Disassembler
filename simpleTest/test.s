f0327dd7aa9d0860e23e1144f30c796cce39060a
Jonathan Pugh <jmp257@zeus.cs.txstate.edu>
2018-05-09 22:05:01 -0500

    Modified simpleTest to show what instructions the disassembler supports

--- simpleTest/test.s
+++ simpleTest/test.s
@@ -1,14 +1,21 @@
+# This test program doesn't do anything interesting, but shows 
+# some of the instructions the disassembler is capable of processing.
 	.global _start
 
 	.text
 _start:
+	mov %eax, %esi
+	mov $0x935, %eax 
+	idiv %ebx
 	imul %rbx
-	mul %rbx
-	add $60, %rax
+	div %rdx
+	mul %edx
+	add $0x60, %rax
 	add %rbx, %rax
-	add $60, %eax
+	add $0x60, %eax
 	add %ebx, %eax
-	mov $43, %ecx
+	mov $0x43, %ecx
 	mov %rax, %rbx
+	add $0x60, %eax
+	push $0xa874f
 	syscall
-	add $60, %eax
